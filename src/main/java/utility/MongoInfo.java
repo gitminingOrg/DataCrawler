9cea6be5418bf154f18f91889c71ee39ac00164b
Yiming Chen <630346810@qq.com>
2016-01-18 10:25:25 +0800

    mongoIp

--- src/main/java/utility/MongoInfo.java
+++ src/main/java/utility/MongoInfo.java
@@ -4,6 +4,6 @@ public class MongoInfo {
 	public static String getMongoServerIp(){
 		String remoteIP = "121.41.118.191";
 		String localhost = "127.0.0.1";
-		return remoteIP;
+		return localhost;
 	}
 }
