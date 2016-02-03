22cd41302f29be6f2e3f43da9b7191681000dd17
miracleaaa <196580784@qq.com>
2016-02-03 21:07:35 +0800

    bug fix

--- src/main/java/metrics22/FileStringReader.java
+++ src/main/java/metrics22/FileStringReader.java
@@ -5,23 +5,32 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FileStringReader {
-	public String getFileContent(String fileName) throws IOException{
-		//init content
+	public String getFileContent(String fileName) throws IOException {
+		// init content
 		String content = null;
-		//read file
-		BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(fileName));
-		byte[] input = new byte[bufferedInputStream.available()];  
-        bufferedInputStream.read(input);  
-        bufferedInputStream.close();
-        content = new String(input);
-		//push into content
+		// read file
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(
+				new FileInputStream(fileName));
+		byte[] input = new byte[bufferedInputStream.available()];
+		bufferedInputStream.read(input);
+		bufferedInputStream.close();
+		content = new String(input);
+		// push into content
 		return content;
 	}
-	
-	public String removeUselessStmt(String content) throws IOException{
-			String result = content.replaceAll("\\}", "");
-			result = result.replaceAll("\\{", "\\{\\}");
-			result = "public class ASTRANGE121NAME{\n" +result+"\n}";
-			return result;
+
+	public String removeUselessStmt(String content) throws IOException {
+		String result = content.replaceAll("\\}", "");
+		result = result.replaceAll("\\{", "\\{\\}");
+		result = "public class ASTRANGE121NAME{\n" + result + "\n}";
+		return result;
+	}
+
+	public String removeUselessStmtMetric5(String content) throws IOException {
+		// String result = content.replaceAll("\\}", "");
+		// result = result.replaceAll("\\{", "\\{\\}");
+		String result = "public class ASTRANGE121NAME{\npublic static void main(String[] args){\n"
+				+ content + "}\n}";
+		return result;
 	}
 }
